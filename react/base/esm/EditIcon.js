afac61a1bbc448939c20a9b62fc76609d69102c1
Markus <hi@mrk.us>
2024-10-29 14:49:10 -0400

    Add icons

--- react/base/esm/EditIcon.js
+++ react/base/esm/EditIcon.js
@@ -15,11 +15,9 @@ function EditIcon({
   }, props), title ? /*#__PURE__*/React.createElement("title", {
     id: titleId
   }, title) : null, /*#__PURE__*/React.createElement("path", {
-    stroke: "#fff",
-    strokeLinecap: "round",
-    strokeLinejoin: "round",
-    strokeWidth: 1.5,
-    d: "m3.957 16.041 3.542-.833 7.744-7.744a.833.833 0 0 0 0-1.178l-1.53-1.53a.833.833 0 0 0-1.179 0L4.79 12.5l-.833 3.541ZM16.04 16.041h-4.583"
+    fillRule: "evenodd",
+    d: "M12.004 4.225a1.583 1.583 0 0 1 2.24 0l1.53 1.53-.508.508.507-.508c.619.619.619 1.621 0 2.24l-.53-.53.53.53-7.744 7.744a.75.75 0 0 1-.359.2l-3.541.833a.75.75 0 0 1-.902-.902l.833-3.542a.75.75 0 0 1 .2-.358l7.744-7.745Zm1.179 1.061a.083.083 0 0 0-.118 0l-.53-.53.53.53-7.593 7.593-.507 2.155 2.154-.507 7.594-7.593a.083.083 0 0 0 0-.118l-1.53-1.53Zm-2.476 10.756a.75.75 0 0 1 .75-.75h4.583a.75.75 0 0 1 0 1.5h-4.583a.75.75 0 0 1-.75-.75Z",
+    clipRule: "evenodd"
   }));
 }
 const ForwardRef = /*#__PURE__*/ React.forwardRef(EditIcon);
---
+++ react/base/esm/StreamIcon.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare const StreamIcon: React.ForwardRefExoticComponent<React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & { title?: string, titleId?: string } & React.RefAttributes<SVGSVGElement>>;
+export default StreamIcon;
